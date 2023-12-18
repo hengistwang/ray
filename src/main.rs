5984e14a615ebeef0070a789ce854582fbeb59ba
hengist <hengistwang666@gmail.com>
2023-12-18 23:31:25 +0800

    add Vec3 struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use core::fmt;
+
 fn main() {
     let image_width = 1024;
     let image_height = 1024;
@@ -19,3 +21,74 @@ fn main() {
     }
     eprintln!("Progress: {}%", 100);
 }
+
+struct Vec3 {
+    x: f64,
+    y: f64,
+    z: f64,
+}
+
+impl Vec3 {
+    fn new() -> Vec3 {
+        Vec3 {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0,
+        }
+    }
+    fn delv(&mut self, r: &Vec3) {
+        self.x -= r.x;
+        self.y -= r.y;
+        self.z -= r.z;
+    }
+    fn addv(&mut self, r: &Vec3) {
+        self.x += r.x;
+        self.y += r.y;
+        self.z += r.z;
+    }
+    fn mulv(&mut self, r: &Vec3) {
+        self.x *= r.x;
+        self.y *= r.y;
+        self.z *= r.z;
+    }
+    fn mulf(&mut self, t: f64) {
+        self.x *= t;
+        self.y *= t;
+        self.z *= t;
+    }
+    fn divf(&mut self, t: f64) {
+        self.mulf(1.0 / t);
+    }
+    fn neg(&mut self) {
+        self.x = -self.x;
+        self.y = -self.y;
+        self.z = -self.z;
+    }
+    fn length_squared(&self) -> f64 {
+        self.x * self.x + self.y * self.y + self.z * self.z
+    }
+    fn dot(&self, r: &Vec3) -> f64 {
+        self.x * r.x + self.y * r.y + self.z * r.z
+    }
+    fn cross(self, r: &Vec3) -> Vec3 {
+        Vec3 {
+            x: self.y * r.z - self.z - r.y,
+            y: self.z * r.x - self.x * r.z,
+            z: self.x * r.y - self.y * r.x,
+        }
+    }
+    fn length(&self) -> f64 {
+        self.length_squared().sqrt()
+    }
+    fn unit_vector(&mut self) {
+        self.divf(self.length())
+    }
+}
+
+impl fmt::Display for Vec3 {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} {} {}", self.x, self.y, self.z)
+    }
+}
+
+//fn write_color()
