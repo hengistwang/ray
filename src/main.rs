18abf1d0e3f7b9f13fd050bc3fd77c425b9cfe72
hengist <hengistwang666@gmail.com>
2023-12-18 22:31:07 +0800

    fucked

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,15 @@
-use rand::{thread_rng, Rng};
-
 fn main() {
-    let image_width = 256;
-    let image_height = 256;
-
+    let image_width = 1024;
+    let image_height = 1024;
     println!("P3");
     println!("{} {}", image_width, image_height);
     println!("255");
 
-    let mut rng = thread_rng();
-    for _ in 0..image_height {
-        for _ in 0..image_width {
-            let r = rng.gen_range(0..=256);
-            let g = rng.gen_range(0..=256);
-            let b = rng.gen_range(0..=256);
+    for i in 0..image_height {
+        for j in (0..image_width).rev() {
+            let r = i / 4;
+            let g = j / 4;
+            let b = 0;
             println!("{} {} {}", r, g, b);
         }
     }
